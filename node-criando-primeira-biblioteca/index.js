3cb54730b394146b098c62003ca2c29c38dc78bb
Filipe Cupini <88508043+FilipeCupini@users.noreply.github.com>
2022-08-13 11:46:31 -0300

    #002 -  Adicionando chalk no projeto

--- node-criando-primeira-biblioteca/index.js
+++ node-criando-primeira-biblioteca/index.js
@@ -1,1 +1,26 @@
-console.log("Primeiros passos")
+import chalk from 'chalk';
+
+const erro = chalk.bold.red;
+const correto = chalk.bold.green;
+let idade = 17;
+
+console.log(chalk.red(`Olá Mundo!`));
+
+const frase = `Essa é a frase para ser exibida`;
+
+function mostrarFrase(string){
+    return string;
+}
+
+console.log(mostrarFrase(frase));
+
+
+function verificaIdade(idade){
+    if(idade >= 18){
+        console.log(correto(`Maior de idade`))
+    } else {
+        console.log(erro(`Menor de idade`))
+    }
+}
+
+verificaIdade(idade)
